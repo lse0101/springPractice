a22bd2a0c62464fd95c8e076836d5050c1adf44b
lse0101 <cmh8207>
2017-02-01 02:10:45 +0900

    init

---
+++ src/main/java/springbook/user/dao/DaoFactory.java
@@ -0,0 +1,29 @@
+package springbook.user.dao;
+
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.SimpleDriverDataSource;
+
+@Configuration
+public class DaoFactory {
+	@Bean
+	public DataSource dataSource() {
+		SimpleDriverDataSource dataSource = new SimpleDriverDataSource ();
+
+		dataSource.setDriverClass(com.mysql.jdbc.Driver.class);
+		dataSource.setUrl("jdbc:mysql://localhost:3307/springbook?characterEncoding=UTF-8");
+		dataSource.setUsername("root");
+		dataSource.setPassword("1234");
+
+		return dataSource;
+	}
+
+	@Bean
+	public UserDao userDao() {
+		UserDao userDao = new UserDao();
+		userDao.setDataSource(dataSource());
+		return userDao;
+	}
+}
